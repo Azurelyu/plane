1ea994c890ffa3431f365ddb4c1678751b9eb886
Azure1yu <1029538915@qq.com>
2018-12-08 12:14:48 +0800

    Initial commit

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,25 @@
+
+
+import sys
+import pygame
+
+def run_game():
+    #initialize game and create a display object
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+    #set background color
+    bg_color = (230,230,230)
+
+
+    #game loop
+    while True:
+        #supervose keyboard and mouse item
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        #fill color
+        screen.fill(bg_color)
+        #visualiaze the window
+        pygame.display.flip()
+run_game()
